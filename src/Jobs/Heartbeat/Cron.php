cdf62c13148896256455fcd8b6c98f8b85308c43
Ross Edlin <rossedlin@gmail.com>
2023-09-18 14:58:44 +0100

    Add Cron + Jobs Heartbeat

---
+++ src/Jobs/Heartbeat/Cron.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Jobs\Heartbeat;
+
+use Exception;
+use Illuminate\Support\Facades\Http;
+
+class Cron
+{
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        try {
+            Http::get(config('heartbeat.cron.url'));
+        } catch (Exception $e) {
+            //Do nothing
+        }
+    }
+}
